1bcf8a10e4a661366b3ced29323eb1cb03c06e88
Halvor Fladsrud Bø <halvor@highered.global>
2020-03-13 13:04:25 +0100

    fixed formatting

--- src/directory/mmap_directory.rs
+++ src/directory/mmap_directory.rs
@@ -485,7 +485,7 @@ mod tests {
     use crate::ReloadPolicy;
     use std::fs;
     use std::sync::atomic::{AtomicUsize, Ordering};
-    use std::time::{Duration};
+    use std::time::Duration;
 
     #[test]
     fn test_open_non_existent_path() {
